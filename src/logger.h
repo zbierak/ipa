70ea6abd5010b9ae8d715ad43466735cbbc30233
zbierak <zbierak@users.noreply.github.com>
2016-07-22 07:55:21 +0200

    Adds some more verbose comments to logger and utils.

--- src/logger.h
+++ src/logger.h
@@ -9,14 +9,28 @@
 #define LOG_LEVEL_ERROR 4
 #define LOG_LEVEL_NONE  5
 
-// by default, all logging is enabled (you can change it in the cmake)
+// By default, all logging is enabled (you can change it in the cmake)
 #ifndef LOG_LEVEL
 #define LOG_LEVEL LOG_LEVEL_DEBUG
 #endif
 
+/**
+ * Removes the path component from an absolute path name, leaving only the name after the
+ * last slash.
+ *
+ * For instance, when you pass "/etc/foo/bar" to this function, it will return "bar".
+ *
+ * @param file_name the absolute file name from which the simple file name should be extracted.
+ * This parameter must contain a NULL-terminated string and must not be NULL.
+ * @return the file name after the last slash found in file_name
+ */
 const char* logger_sanitize_file_name(const char* file_name);
 
-// logger macros overwriting the log level check
+// Logger macros overwriting the log level check.
+// They optionally include also the call location, but we need that info only in debug mode,
+// since the errors (via LOG_ERROR) are presented to the end users on different events (e.g.
+// whenever the device cannot be detected etc.), hence the need for the plain text messages
+// for release.
 #ifdef LOG_MODE_DEBUG
 #define LOG_DEBUG_OVERWRITE(format, ARG...) \
 	do { printf("[DEBUG] " format " [%s@%s:%d]\n", ##ARG, __func__, logger_sanitize_file_name(__FILE__), __LINE__); } while(0)
@@ -33,7 +47,7 @@ const char* logger_sanitize_file_name(const char* file_name);
 #define LOG_ERROR_OVERWRITE(format, ARG...) do { fprintf(stderr, format "\n", ##ARG); } while(0)
 #endif
 
-// logger macros which are enabled/disabled by the log level
+// Logger macros which are enabled/disabled by the log level
 #if LOG_LEVEL <= LOG_LEVEL_DEBUG
 #define LOG_DEBUG(format, ARG...) LOG_DEBUG_OVERWRITE(format, ##ARG)
 #else
--- src/utils.h
+++ src/utils.h
@@ -2,6 +2,8 @@
 
 #include <string.h>
 
+// A null-safe variant of strdup
 #define STRDUP(x) (((x) == NULL) ? NULL : strdup(x))
 
+// A null-safe variant of strstr
 #define STRSTR(haystack, needle) (((haystack) == NULL || (needle) == NULL) ? NULL : strstr(haystack, needle))
